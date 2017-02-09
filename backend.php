2353918ccc85498ef40fafd0a67ea805e6207430
sara-h-chen <deutscher71112@gmail.com>
2017-02-09 14:04:48 +0000

    Recreate table with unique values

--- backend.php
+++ backend.php
@@ -72,7 +72,7 @@ if ($method === 'GET') {
                 $scout_id = ($scout->scout_id);
                 $date_of_payment = ($scout->payment_date);
                 $amount_paid = ($scout->payment_amount);
-                $update = $group_dbs->prepare("INSERT INTO payment_records(scout_id, payment_date, payment_amount) VALUES (:id, :payment_date, :payment_amount)");
+                $update = $group_dbs->prepare("INSERT INTO payment_records(scout_id, payment_date, payment_amount) VALUES (:id, :payment_date, :payment_amount) WHERE NOT EXISTS (SELECT scout_id, payment_date, payment_amount FROM payment_records WHERE scout_id=:id AND payment_date=:payment_date AND payment_amount=:payment_amount)");
                 $update->bindValue(':id', $scout_id, PDO::PARAM_INT);
                 $update->bindValue(':payment_date', $date_of_payment, PDO::PARAM_STR);
                 $update->bindValue(':payment_amount', $amount_paid);
@@ -86,7 +86,7 @@ if ($method === 'GET') {
             if (!empty($scout->scout_id) && !empty($scout->payment_description)) {
                 $scout_id = ($scout->scout_id);
                 $description = ($scout->payment_description);
-                $update = $group_dbs->prepare("INSERT INTO manual_matches(scout_id, payment_description) VALUES (:id, :description)");
+                $update = $group_dbs->prepare("INSERT INTO OR IGNORE manual_matches(scout_id, payment_description) VALUES (:id, :description)");
                 $update->bindValue(':id', $scout_id, PDO::PARAM_INT);
                 $update->bindValue(':description', $description, PDO::PARAM_STR);
                 $update->execute();
