f603334f0403aed2d7171ce4e78ba266580130d2
sara-h-chen <deutscher71112@gmail.com>
2017-02-09 17:38:18 +0000

    Disallow duplicate entries

--- backend.php
+++ backend.php
@@ -72,7 +72,7 @@ if ($method === 'GET') {
                 $scout_id = ($scout->scout_id);
                 $date_of_payment = str_replace('/','-',($scout->payment_date));
                 $amount_paid = ($scout->payment_amount);
-                $update = $group_dbs->prepare("INSERT INTO payment_records(scout_id, payment_date, payment_amount) VALUES (:id, :payment_date, :payment_amount) WHERE NOT EXISTS (SELECT scout_id, payment_date, payment_amount FROM payment_records WHERE scout_id=:id AND payment_date=:payment_date AND payment_amount=:payment_amount)");
+                $update = $group_dbs->prepare("INSERT INTO payment_records(scout_id, payment_date, payment_amount) SELECT (:id, :payment_date, :payment_amount) WHERE NOT EXISTS (SELECT 1 FROM payment_records WHERE scout_id=:id AND payment_date=:payment_date AND payment_amount=:payment_amount)");
                 $update->bindValue(':id', $scout_id, PDO::PARAM_INT);
                 $update->bindValue(':payment_date', $date_of_payment, PDO::PARAM_STR);
                 $update->bindValue(':payment_amount', $amount_paid);
