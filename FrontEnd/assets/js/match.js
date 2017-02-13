dc07eafbd5c59fdd0ec73b9f8336058512a95f86
TheSmallSloth <tom1432@live.com>
2017-02-13 15:03:30 +0000

    Update match.js

--- FrontEnd/assets/js/match.js
+++ FrontEnd/assets/js/match.js
@@ -15,16 +15,6 @@ $.get("http://community.dur.ac.uk/sara.h.chen/team2-cep/backend.php",
 			$("#upload").append('<form id="fileForm"><input type="file" id="inputFile" name="csv" accept=".csv"></form><img src="assets/imgs/Upload.png" alt="upload" id="uploadIcon">');
 			$('#fileForm').hide();
 			
-			$('#upload').mouseenter(function(element) {
-				document.getElementById('uploadIcon').src="assets/imgs/UploadHovered.png";
-				$('#fileForm').toggle();
-			});
-
-			$('#upload').mouseleave(function(element){
-				document.getElementById('uploadIcon').src="assets/imgs/Upload.png";
-				$('#fileForm').toggle();
-			});
-			
 			$('input[type=file]').change(function(element) {
 				var fileReader = new FileReader();
 				fileReader.onload = function(event) {
